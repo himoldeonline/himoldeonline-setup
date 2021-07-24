0b23690922b3ad70e24ab04d255f58779c14e723
emilbratt <emilbratt@gmail.com>
2021-07-24 15:58:16 +0200

    putting variables for repositories in its own file

---
+++ setup/lib/repositories.sh
@@ -0,0 +1,18 @@
+# URLS, BRANCH and TAGS for our repositories
+
+# Open edX source-code
+GIT_URL_EDX_PLATFORM='git@github.com:himoldeonline/edx-platform.git'
+TAG_EDX_PLATFORM='tags/open-release/lilac.1'
+
+# openedx-instructions source-code
+GIT_URL_OPENEDX_INSTRUCTIONS='git@github.com:himoldeonline/openedx-instructions.git'
+
+# openedx-dev source-code
+GIT_URL_OPENEDX_DEV='git@github.com:himoldeonline/openedx-dev.git'
+BRANCH_OPENEDX_DEV='setup'
+
+# openedx-tibetheme source-code
+GIT_URL_TIBETHEME='git@github.com:himoldeonline/openedx-tibetheme.git'
+
+# tutor source-code
+GIT_URL_TUTOR='https://github.com/overhangio/tutor'
