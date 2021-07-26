92308dec38cb18e4c853268141e3f8f101fd739b
emilbratt <emilbratt@gmail.com>
2021-07-26 10:27:03 +0200

    manually add ssh-key to ssh-agent to avoid issues if ssh-agent is not loaded into session either through .*rc or init

--- setup/4_clone.sh
+++ setup/4_clone.sh
@@ -3,19 +3,19 @@
 _log_msg 'Cloning Repositories'
 
 _info_cloning "$GIT_URL_EDX_PLATFORM -> \e[0;91m$EDX_PLATFORM_ROOT/\e[0m"
-git clone $GIT_URL_EDX_PLATFORM $EDX_PLATFORM_ROOT 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
-cd $EDX_PLATFORM_ROOT git fetch --all --tags  1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
-cd $EDX_PLATFORM_ROOT && git checkout $TAG_EDX_PLATFORM  1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
+ssh-agent sh -c "ssh-add $SSH_KEY; git clone $GIT_URL_EDX_PLATFORM $EDX_PLATFORM_ROOT" &>> $_LOG_FILE && _info_ok || _log_tail_exit
+cd $EDX_PLATFORM_ROOT git fetch --all --tags  &>> $_LOG_FILE && _info_ok || _log_tail_exit
+cd $EDX_PLATFORM_ROOT && git checkout $TAG_EDX_PLATFORM  &>> $_LOG_FILE && _info_ok || _log_tail_exit
 
 _info_cloning "$GIT_URL_OPENEDX_INSTRUCTIONS -> \e[0;91m$INSTRUCTION_ROOT/\e[0m"
-git clone $GIT_URL_OPENEDX_INSTRUCTIONS $INSTRUCTION_ROOT 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
+ssh-agent sh -c "ssh-add $SSH_KEY; git clone $GIT_URL_OPENEDX_INSTRUCTIONS $INSTRUCTION_ROOT" &>> $_LOG_FILE && _info_ok || _log_tail_exit
 
 _info_cloning "$GIT_URL_OPENEDX_DEV -> \e[0;91m$OPENEDX_DEV_ROOT/\e[0m"
-git clone $GIT_URL_OPENEDX_DEV $OPENEDX_DEV_ROOT 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
-cd $OPENEDX_DEV_ROOT && git checkout $BRANCH_OPENEDX_DEV 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
+ssh-agent sh -c "ssh-add $SSH_KEY; git clone $GIT_URL_OPENEDX_DEV $OPENEDX_DEV_ROOT" &>> $_LOG_FILE && _info_ok || _log_tail_exit
+cd $OPENEDX_DEV_ROOT && git checkout $BRANCH_OPENEDX_DEV &>> $_LOG_FILE  && _info_ok || _log_tail_exit
 
 _info_cloning "$GIT_URL_TIBETHEME -> \e[0;91m$TIBE_THEME_ROOT/\e[0m"
-git clone $GIT_URL_TIBETHEME $TIBE_THEME_ROOT 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
+ssh-agent sh -c "ssh-add $SSH_KEY; git clone $GIT_URL_TIBETHEME $TIBE_THEME_ROOT" &>> $_LOG_FILE && _info_ok || _log_tail_exit
 
 _info_cloning "$GIT_URL_TUTOR -> \e[0;91m$TUTOR_ROOT/\e[0m"
-git clone $GIT_URL_TUTOR $TUTOR_ROOT 1> /dev/null 2>> $_LOG_FILE && _info_ok || _log_tail_exit
+ssh-agent sh -c "ssh-add $SSH_KEY; git clone $GIT_URL_TUTOR $TUTOR_ROOT" &>> $_LOG_FILE && _info_ok || _log_tail_exit
