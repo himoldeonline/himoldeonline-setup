b96723e4e62306d392887bcae0ace56aca27ad9b
emilbratt <emilbratt@gmail.com>
2021-07-28 11:58:07 +0200

    fixing error:  tee: /etc/apt/sources.list.d/docker.list: Permission denied..  by adding sudo

--- setup/distro/ubuntu.sh
+++ setup/distro/ubuntu.sh
@@ -69,6 +69,6 @@ _get_docker () {
 	_info_installation 'Docker'
   _continue
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io  &>> $_LOG_FILE || _log_tail_exit
 }
