7c027967a1776b3f84a114046e3cb4c87ed1b4e2
emilbratt <emilbratt@gmail.com>
2021-08-07 21:46:30 +0200

    move docker group validation and make it run every time

--- setup/2_installation.sh
+++ setup/2_installation.sh
@@ -32,50 +32,49 @@ if ! _running_wsl; then
     # exit if enabling docker daemon failed
     _log_tail_exit
 
-  # ..validate member of docker group
-  if ! _in_group docker; then
-    _log_msg 'Add user to docker group'
-    _add_user_to_group $USER docker &>> $_LOG_FILE || _log_tail_exit
-  fi
-
   _info_ok 'ok'
 fi
 
+# ..validate member of docker group
+if ! _in_group docker; then
+  _log_msg 'Add user to docker group'
+  _add_user_to_group $USER docker &>> $_LOG_FILE || _log_tail_exit
+fi
 
 # OS-independent installations
-_get_pyenv () {
-  # if pyenv is installed and python is installed, jump out of function
-  if _has_command pyenv; then
-      if _has_command python; then return 0; fi
-  fi
-
-  if ! _has_command pyenv; then
-    PYTHON_VERSION="3.9.6"
-    _yes_or_no "Do you want to install Pyenv with Python $PYTHON_VERSION [Optional]" || eval '_info_ok "skipping" && return 0'
-
-    _log_msg "Installing pyenv and $PYTHON_VERSION"
-    _install_packages 'pyenv'
-    _info_installation "Installing pyenv and python $PYTHON_VERSION"
-    eval "curl https://pyenv.run/ | bash" &>> $_LOG_FILE || _log_tail_exit
-
-    # after install, if not call-able from $PATH
-    if ! _has_command pyenv; then
-      _append_to_profile 'export PATH="$HOME/.pyenv/bin:$PATH"'
-      export PATH="$HOME/.pyenv/bin:$PATH"
-    fi
-    _append_to_profile 'eval "$(pyenv init -)"'
-    _append_to_profile 'eval "$(pyenv virtualenv-init -)"'
-
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-    pyenv install -v $PYTHON_VERSION &>> $_LOG_FILE || _log_tail_exit
-    pyenv global $PYTHON_VERSION &>> $_LOG_FILE || _log_tail_exit
-    _info_ok 'ok'
-  fi
-
-
-}
-_get_pyenv
+# _get_pyenv () {
+#   # if pyenv is installed and python is installed, jump out of function
+#   if _has_command pyenv; then
+#       if _has_command python; then return 0; fi
+#   fi
+#
+#   if ! _has_command pyenv; then
+#     PYTHON_VERSION="3.9.6"
+#     _yes_or_no "Do you want to install Pyenv with Python $PYTHON_VERSION [Optional]" || eval '_info_ok "skipping" && return 0'
+#
+#     _log_msg "Installing pyenv and $PYTHON_VERSION"
+#     _install_packages 'pyenv'
+#     _info_installation "Installing pyenv and python $PYTHON_VERSION"
+#     eval "curl https://pyenv.run/ | bash" &>> $_LOG_FILE || _log_tail_exit
+#
+#     # after install, if not call-able from $PATH
+#     if ! _has_command pyenv; then
+#       _append_to_profile 'export PATH="$HOME/.pyenv/bin:$PATH"'
+#       export PATH="$HOME/.pyenv/bin:$PATH"
+#     fi
+#     _append_to_profile 'eval "$(pyenv init -)"'
+#     _append_to_profile 'eval "$(pyenv virtualenv-init -)"'
+#
+#     eval "$(pyenv init -)"
+#     eval "$(pyenv virtualenv-init -)"
+#     pyenv install -v $PYTHON_VERSION &>> $_LOG_FILE || _log_tail_exit
+#     pyenv global $PYTHON_VERSION &>> $_LOG_FILE || _log_tail_exit
+#     _info_ok 'ok'
+#   fi
+#
+#
+# }
+# _get_pyenv
 
 
 _get_docker_compose () {
