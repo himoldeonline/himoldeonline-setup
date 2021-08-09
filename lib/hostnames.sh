b787d7d6df3241f1ea3c8e445c66126ed289ce08
emilbratt <emilbratt@gmail.com>
2021-08-09 19:11:58 +0200

    fix correct hostname

--- lib/hostnames.sh
+++ lib/hostnames.sh
@@ -1,4 +1,4 @@
 # hostnames used for himoldeonline
 _HOST_OPEN_EDX_LMS='local.overhang.io'
 _HOST_OPEN_EDX_CMS='studio.local.overhang.io'
-_HOST_CRAFT_CMS_LMS='himolde.nitro'
+_HOST_DEV_WEB_PORTAL='himoldeonline-portal.nitro'
