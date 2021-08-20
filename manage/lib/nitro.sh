502c70c8c7a627bbff2734ac18cc5d38c9da1299
emilbratt <emilbratt@gmail.com>
2021-08-20 19:59:15 +0200

    add support for installation of nitro for macos

--- manage/lib/nitro.sh
+++ manage/lib/nitro.sh
@@ -1,94 +1,102 @@
 # nitro installation and nitro commands
 
-_install_nitro () {
+function _install_nitro () {
   # this is a modified version of nitros official installation script
   # the script was fetched from here: http://installer.getnitro.sh
   # and is documented here: https://craftcms.com/docs/nitro/2.x/installation.html
   export TEMP_FOLDER="$HOME/himoldeonline/temp_nitro_extract"
-  export DOWNLOAD_SUFFIX=""
-  export DOWNLOAD_ARCH=""
+  export FINAL_DIR_LOCATION="/usr/local/bin"
+  export FINAL_BINARY_LOCATION="/usr/local/bin/nitro"
   export DOWNLOAD_ZIP_EXTENSION=".tar.gz"
   export EXECUTABLE_NAME="nitro"
 
-  function checkPlatform {
-    DOWNLOAD_SUFFIX="_linux"
-    arch=$(uname -m)
-    case $arch in
-    "x86_64")
-      DOWNLOAD_ARCH="_x86_64"
-      ;;
-    "aarch64")
-      DOWNLOAD_ARCH="_aarch64"
-      ;;
-    "arm64")
-      DOWNLOAD_ARCH="_arm64"
-      ;;
-    *)
-      ;;
-    esac
-  }
-  _log_msg 'Finding cpu architecture'
-  checkPlatform || exit 1
 
-  _log_msg 'Finding the latest nitro version'
-  version=$(curl -s https://api.github.com/repos/craftcms/nitro/releases | grep -i -m 1 tag_name | head -1 | sed 's/\("tag_name": "\(.*\)",\)/\2/' | tr -d '[:space:]')
-  if [ ! "$version" ]; then
-    _log_msg 'Could not get the latest nitro version'
-    exit 1
+  _log_msg 'Getting platform and cpu architecture'
+  uname=$(uname)
+  if [[ $uname == Darwin ]]; then DOWNLOAD_SUFFIX="_darwin"
+  elif [[ $uname == Linux ]]; then DOWNLOAD_SUFFIX="_linux"
+  fi
+
+  arch=$(uname -m)
+  if [[ $arch == x86_64 ]]; then DOWNLOAD_ARCH="_x86_64"
+  elif [[ $arch == aarch64 ]]; then DOWNLOAD_ARCH="_aarch64"
+  elif [[ $arch == arm64 ]]; then DOWNLOAD_ARCH="_arm64"
+  fi
+
+
+  _log_msg 'Getting the latest nitro version'
+  _URL_NITRO_RELEASES=https://api.github.com/repos/craftcms/nitro/releases
+  version=$(curl -s $_URL_NITRO_RELEASES | grep -i -m 1 tag_name | head -1 | sed 's/\("tag_name": "\(.*\)",\)/\2/' | tr -d '[:space:]')
+
+  if [[ ! "$version" ]]; then
+    _log_msg 'Could not get the latest nitro version'; _log_tail_exit
   fi
 
   function checkHash {
-    sha_cmd="sha256sum"
+
+    _URL_NITRO_RELEASES_CHECKSUMS=https://github.com/craftcms/nitro/releases/download
     fileName=nitro_$2_checksums.txt
     filePath="$HOME/himoldeonline/temp_nitro_extract/$fileName"
-    checksumUrl=https://github.com/craftcms/nitro/releases/download/$version/$fileName
+    checksumUrl="$_URL_NITRO_RELEASES_CHECKSUMS"/"$version"/"$fileName"
     targetFile=$3/$fileName
+    if _is_command sha256sum; then
+      sha_cmd="sha256sum"
+    else
+      shaCmd="shasum -a 256"
+    fi
 
-    if [ -x "$(command -v $shaCmd)" ]; then
+    if [[ -x "$(command -v $shaCmd)" ]]; then
       # download the checksum file.
       (curl -sLS "$checksumUrl" --output "$targetFile")
       # Run the sha command against the zip and grab the hash from the first segment.
       zipHash="$($shaCmd "$1" | cut -d' ' -f1 | tr -d '[:space:]')"
-      # See if the checksum we calculated matches a result in the checksum file.
+      # See if the file we calculated matches a result in the checksum file.
       checkResultFileName=$(sed -n "s/^$zipHash  //p" "$filePath")
+      # Make sure the file names match up.
+      if [[ "$4" != "$checkResultFileName" ]]; then
+        rm "$1"; _log_error "Checksums do not checkout for $1"
+      fi
       # don't need this anymore
       rm "$filePath"
-      # Make sure the file names match up.
-      if [ "$4" != "$checkResultFileName" ]; then
-        rm "$1"
-        _log_msg "Checksums do not checkout for $filePath"
-        exit 1
-      fi
     fi
   }
 
+
   targetTempFolder="$HOME/himoldeonline/temp_nitro_extract"
-  mkdir -p "$targetTempFolder"
+  mkdir -p "$targetTempFolder" && _log_msg "Creating $targetTempFolder"
 
   fileName=nitro$DOWNLOAD_SUFFIX$DOWNLOAD_ARCH$DOWNLOAD_ZIP_EXTENSION
   packageUrl=https://github.com/craftcms/nitro/releases/download/$version/"$fileName"
   targetZipFile="$targetTempFolder/$fileName"
 
   _log_msg "Downloading package $packageUrl to $targetZipFile"
-  curl -sSL "$packageUrl" --output "$targetZipFile" || exit 1
+  curl -sSL "$packageUrl" --output "$targetZipFile" ||
+    _log_error "Could not download $targetZipFile from $packageUrl"
 
   _log_msg "Extracting $targetZipFile downloaded nitro tarball to $targetTempFolder"
-  tar xzf "$targetZipFile" -C "$targetTempFolder" || exit 1
+  tar xzf "$targetZipFile" -C "$targetTempFolder"
 
-  _log_msg 'Running checksum to validate integrity of downloaded files'
-  checkHash "$targetZipFile" "$version" "$targetTempFolder" "$fileName" || exit 1
+  _log_msg "Running checksum to validate integrity of $targetZipFile files"
+  checkHash "$targetZipFile" "$version" "$targetTempFolder" "$fileName"
 
-  _log_msg 'Make nitro it executable'
-  chmod +x "$targetTempFolder/nitro" || exit 1
+  _log_msg "Making $targetTempFolder"/"$EXECUTABLE_NAME executable"
+  chmod +x "$targetTempFolder"/"$EXECUTABLE_NAME" || _log_tail_exit
 
-  _log_msg 'Moving nitro executable to final location'
-  sudo mv "$targetTempFolder/nitro" "/usr/local/bin/nitro" || exit 1
+  if ! _dir_exist $FINAL_DIR_LOCATION; then
+    sudo mkdir -p $FINAL_DIR_LOCATION && _log_msg "Creating directory $FINAL_DIR_LOCATION"
+  fi
 
-  _log_msg "Removing $targetTempFolder"
-  rm -rf "$targetTempFolder"
-  echo 'Done'
+  _log_msg "Moving $targetTempFolder/$EXECUTABLE_NAME to $FINAL_BINARY_LOCATION"
+  sudo mv "$targetTempFolder"/"$EXECUTABLE_NAME" "$FINAL_BINARY_LOCATION" ||
+    _log_error "Could not move $targetTempFolder/nitro to $FINAL_BINARY_LOCATION"
+
+  # clean up
+  rm -rf "$targetTempFolder" && _log_msg "Removing $targetTempFolder"
+
+  _log_msg "Nitro $version has been installed to $FINAL_DIR_LOCATION"
+
+  echo "Nitro $version has been installed to $FINAL_DIR_LOCATION"
 }
-
 
 
 _nitro_init_environment () {
